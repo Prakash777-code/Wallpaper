096673b424907dccde3b8961f9a2cab46ef3ec20
Prakash Kumar <Prakash Kumar>
2026-08-02 00:25:12 +0530

    Changed status frontend api

--- src/services/Wallpapers/loadMore.ts
+++ src/services/Wallpapers/loadMore.ts
@@ -1,34 +1,7 @@
 export const checkLoadMore = async () => {
-  let res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/status`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/status`, {
     credentials: "include",
   });
-  if (res.status === 401) {
-    const getRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`, {
-      method: "POST",
-      credentials:"include"
-    });
-    if (getRes.status === 401) {
-      return {
-        ok: false,
-        status: 401,
-        data: {
-          message: "Unauthorised",
-        },
-      };
-    }
-
-    if (!getRes.ok) {
-      const data = await getRes.json();
-      return {
-        ok: false,
-        status: getRes.status,
-        data,
-      };
-    }
-    res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/status`,{
-        credentials:"include"
-    });
-  }
   const data = await res.json();
 
   return {
