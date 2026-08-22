7ee39f166dff416ed9ae738c3f30cb47e8a6ced8
Prakash Kumar <Prakash Kumar>
2026-08-22 19:43:21 +0530

    Make download page card responsive

--- src/pages/downloads.tsx
+++ src/pages/downloads.tsx
@@ -128,7 +128,7 @@ export default function Downloads() {
             </button>
           </div>
         ) : (
-          <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="grid grid-cols-2 gap-3 sm:gap-5 lg:grid-cols-3 xl:grid-cols-4">
             {downloadedWallpapers.map((wallpaper) => (
               <div
                 key={wallpaper.wallpaperId}
